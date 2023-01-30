f8ff4d15df4ba9bfe1816bde4c19f48b84e07a2e
LiliaSlabka <liaslabka@gmail.com>
2023-01-31 00:01:53 +0200

    02-lightbox-done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,19 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery')
+
+const renderGalleryList = (pictures) => pictures.reduce((acc, picture) => acc +
+    `<a class="gallery__item" href=${picture.original}>
+  <img class="gallery__image" src=${picture.preview} alt=${picture.description} />
+</a> `, '');
+
+const addGalleryItems = (string) => {
+    galleryEl.insertAdjacentHTML('beforeend', renderGalleryList(galleryItems))
+};
+
+addGalleryItems(renderGalleryList(galleryItems));
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData:"alt",
+    captionDelay: 250,
+});
